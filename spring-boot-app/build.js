87cc0f80fbafb55e0eb39d3529817e50ec39b93c
inpercima <inpercima@gmail.com>
2017-01-30 22:27:44 +0100

    Update scripts
    * change mysql version from 5.6 to 5.7
    * change tomcat version from 8.0.39 to 8.5.11
    * add phpmyadmin

--- spring-boot-app/build.js
+++ spring-boot-app/build.js
@@ -1,3 +1,3 @@
 var shjs = require('shelljs');
 
-shjs.exec('docker build -t inpercima/ubuntu-base .');
+shjs.exec('docker build -t inpercima/spring-boot-app .');
--- spring-boot-app/run.js
+++ spring-boot-app/run.js
@@ -1,4 +1,4 @@
 var shjs = require('shelljs');
 
 shjs.exec('docker rm -f inpercima_sba');
-shjs.exec('docker run --name inpercima_sba -d -i -t -p 8080:8080 --link mysql5 inpercima/sba');
+shjs.exec('docker run --name inpercima_sba -d -it -p 8080:8080');
